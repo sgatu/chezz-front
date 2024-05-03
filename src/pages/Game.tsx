25c21f269eb5b871f52bab031407e70b79065f2f
Stefan Gatu <stefan.gatu@gmail.com>
2024-05-03 23:04:13 +0100

    fix websocket connect

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -37,7 +37,7 @@ export default function Game() {
 
   useEffect(() => {
     if (getGameStatus.game?.id) {
-      setWsUrl("ws://" + import.meta.env.BASE_URL.replace("http://", "").replace("https://", "") + "/play/" + getGameStatus.game?.id)
+      setWsUrl(import.meta.env.BASE_URL.replace("http://", "ws://").replace("https://", "wss://") + "/play/" + getGameStatus.game?.id)
       setCurrentGame(getGameStatus.game)
     }
   }, [getGameStatus]);
